5656f4c133ab4f1d42a706db3cedd0ff59f5e741
Navid Tavakoli <parseram.user4@gmail.com>
2024-06-11 22:25:50 +0330

    12

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -8,7 +8,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "8ea08ea2444cc58ec5807fd7669e488f",
-"assets/NOTICES": "572441f253fbf8c3ac4639c17510d97e",
+"assets/NOTICES": "5a545e0ee6212c435c824a9c61c1598f",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "canvaskit/canvaskit.js": "c86fbd9e7b17accae76e5ad116583dc4",
@@ -27,9 +27,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "da313fa4e9271e16293d3e796b67dd4c",
-"/": "da313fa4e9271e16293d3e796b67dd4c",
-"main.dart.js": "2aa91cc13afef8de3ff88bc6d21c3cbf",
+"index.html": "15eafaa3c3323469472d2b88a202abe9",
+"/": "15eafaa3c3323469472d2b88a202abe9",
+"main.dart.js": "26df6f237a23ba32a4295eacd3a4d06a",
 "manifest.json": "4e214a5afbfeec71911ab5aa68e21773",
 "version.json": "cd1d82760c387149baed2699d5307a2c"};
 // The application shell files that are downloaded before a service worker can
